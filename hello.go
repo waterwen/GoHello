6d4a554861611240dc2a6a9939acbcc29443c800
Laura Wang <laura_wang@hiiir.com>
2017-12-15 15:48:18 +0800

    Import stringutil package from remote repository.

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/waterwen/GoStringutil"
+)
 
 func main(){
-	fmt.Printf("Hello, world!")
+	fmt.Printf("Hello, world!\n")
+	fmt.Printf(stringutil.Reverse("Hello, world!"))
 }
